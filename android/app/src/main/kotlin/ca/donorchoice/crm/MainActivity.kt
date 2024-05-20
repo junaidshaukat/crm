10ecc1f8d1a55d0142dc4608ff2e230a4b18189e
junaidshaukat <junaidshaukat123@gmail.com>
2024-05-20 10:36:43 +0500

    update

--- android/app/src/main/kotlin/ca/donorchoice/crm/MainActivity.kt
+++ android/app/src/main/kotlin/ca/donorchoice/crm/MainActivity.kt
@@ -2,4 +2,13 @@ package ca.donorchoice.crm
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.engine.FlutterEngine
+import androidx.annotation.NonNull;
+
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
