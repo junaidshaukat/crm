a817b9ec26670bfaf7d67478e2c91cf1f4846f29
junaidshaukat <junaidshaukat123@gmail.com>
2024-05-30 03:34:31 +0500

    update

--- lib/presentation/signin/controller/controller.dart
+++ lib/presentation/signin/controller/controller.dart
@@ -9,8 +9,8 @@ class SigninController extends GetxController {
   EnvConfig env = EnvConfig();
 
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+  TextEditingController emailController = TextEditingController(text:emailText);
+  TextEditingController passwordController = TextEditingController(text: passwordText);
 
   Rx<bool> isShowPassword = true.obs;
 
