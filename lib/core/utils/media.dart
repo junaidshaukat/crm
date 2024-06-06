d7ab12fbbfceb60b261c525af8372dd7b414a00d
junaidshaukat <junaidshaukat123@gmail.com>
2024-06-06 12:17:11 +0500

    update

--- lib/core/utils/media.dart
+++ lib/core/utils/media.dart
@@ -31,7 +31,7 @@ class Media {
     VideoPlayerValue? video;
     Size resolution = Size.zero;
 
-    List videos = ['mp4'];
+    List videos = ['mp4', 'mov'];
     List images = ['jpg', 'jpeg', 'png', 'bmp', 'gif'];
 
     File file = param;
