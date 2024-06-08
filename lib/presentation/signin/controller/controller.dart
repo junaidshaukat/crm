f6ec5eb376bf6929e96de787779e2b5f79a09a3d
junaidshaukat <junaidshaukat123@gmail.com>
2024-06-09 02:13:43 +0500

    update

--- lib/presentation/signin/controller/controller.dart
+++ lib/presentation/signin/controller/controller.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 import '/core/app_export.dart';
 
-String emailText = "shahzad.khan@mervice.ca";
-String passwordText = "Donatenow123\$";
-
 class SigninController extends GetxController {
   Props props = Props();
   EnvConfig env = EnvConfig();
 
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  TextEditingController emailController =
-      TextEditingController(text: emailText);
-  TextEditingController passwordController =
-      TextEditingController(text: passwordText);
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   Rx<bool> isShowPassword = true.obs;
 
