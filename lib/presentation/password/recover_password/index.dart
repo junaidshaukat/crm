f08f6f19a9967abfe9b85755a90f485bee9f9f1b
junaidshaukat <junaidshaukat123@gmail.com>
2024-06-09 02:26:35 +0500

    update

--- lib/presentation/password/recover_password/index.dart
+++ lib/presentation/password/recover_password/index.dart
@@ -36,69 +36,71 @@ class RecoverPasswordScreen extends StatelessWidget {
         centerTitle: true,
         title: AppbarTitle(text: "recover_password".tr),
       ),
-      body: Container(
-        width: double.maxFinite,
-        padding: EdgeInsets.symmetric(vertical: 37.v),
-        child: Form(
-          key: controller.formKey,
-          child: Column(
-            children: [
-              Container(
-                width: 321.h,
-                margin: EdgeInsets.symmetric(horizontal: 26.h),
-                child: Text(
-                  "enter_the_email".tr,
-                  maxLines: 4,
-                  overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: appTheme.black900,
-                    fontSize: 16.fSize,
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.w400,
+      body: SafeArea(
+        child: Container(
+          width: double.maxFinite,
+          padding: EdgeInsets.symmetric(vertical: 37.v),
+          child: Form(
+            key: controller.formKey,
+            child: Column(
+              children: [
+                Container(
+                  width: 321.h,
+                  margin: EdgeInsets.symmetric(horizontal: 26.h),
+                  child: Text(
+                    "enter_the_email".tr,
+                    maxLines: 4,
+                    overflow: TextOverflow.ellipsis,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: appTheme.black900,
+                      fontSize: 16.fSize,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 40.v),
-              CustomTextFormField(
-                width: 325.h,
-                controller: controller.emailController,
-                hintText: "email_address".tr,
-                textInputAction: TextInputAction.search,
-                alignment: Alignment.center,
-                prefix: Container(
-                  margin: EdgeInsets.fromLTRB(20.h, 13.v, 10.h, 13.v),
-                  child: CustomImageView(
-                    imagePath: "email".icon.svg,
-                    height: 24.adaptSize,
-                    width: 24.adaptSize,
+                SizedBox(height: 40.v),
+                CustomTextFormField(
+                  width: 325.h,
+                  controller: controller.emailController,
+                  hintText: "email_address".tr,
+                  textInputAction: TextInputAction.search,
+                  alignment: Alignment.center,
+                  prefix: Container(
+                    margin: EdgeInsets.fromLTRB(20.h, 13.v, 10.h, 13.v),
+                    child: CustomImageView(
+                      imagePath: "email".icon.svg,
+                      height: 24.adaptSize,
+                      width: 24.adaptSize,
+                    ),
+                  ),
+                  prefixConstraints: BoxConstraints(maxHeight: 50.v),
+                  contentPadding:
+                      EdgeInsets.only(top: 16.v, right: 30.h, bottom: 16.v),
+                  borderDecoration: TextFormFieldStyleHelper.fillGrayTL5,
+                  filled: true,
+                  fillColor: appTheme.gray10001,
+                  validator: Validator.email,
+                ),
+                SizedBox(height: 16.v),
+                Obx(
+                  () => CustomElevatedButton(
+                    text: controller.props.useState.value == UseState.none
+                        ? "send_instructions".tr
+                        : "",
+                    leftIcon: controller.props.useState.value == UseState.none
+                        ? null
+                        : CustomProgressButton(
+                            lable: 'processing'.tr,
+                          ),
+                    margin: EdgeInsets.symmetric(horizontal: 25.h),
+                    onPressed: forgetPassword,
                   ),
                 ),
-                prefixConstraints: BoxConstraints(maxHeight: 50.v),
-                contentPadding:
-                    EdgeInsets.only(top: 16.v, right: 30.h, bottom: 16.v),
-                borderDecoration: TextFormFieldStyleHelper.fillGrayTL5,
-                filled: true,
-                fillColor: appTheme.gray10001,
-                validator: Validator.email,
-              ),
-              SizedBox(height: 16.v),
-              Obx(
-                () => CustomElevatedButton(
-                  text: controller.props.useState.value == UseState.none
-                      ? "send_instructions".tr
-                      : "",
-                  leftIcon: controller.props.useState.value == UseState.none
-                      ? null
-                      : CustomProgressButton(
-                          lable: 'processing'.tr,
-                        ),
-                  margin: EdgeInsets.symmetric(horizontal: 25.h),
-                  onPressed: forgetPassword,
-                ),
-              ),
-              SizedBox(height: 49.v),
-            ],
+                SizedBox(height: 49.v),
+              ],
+            ),
           ),
         ),
       ),
