1c090377fcf8809526eaf89a581ccaaf514c2f0b
junaidshaukat <junaidshaukat123@gmail.com>
2024-06-13 03:14:35 +0500

    update

--- lib/core/utils/status_bar.dart
+++ lib/core/utils/status_bar.dart
@@ -3,17 +3,17 @@ import '/core/app_export.dart';
 
 class StatusBar {
   static void setSystemUIOverlayStyle(bool green) {
-    SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarColor: green ? appTheme.green : appTheme.gray,
-        systemNavigationBarColor: appTheme.gray,
-        systemNavigationBarDividerColor: appTheme.gray,
-        statusBarBrightness: green ? Brightness.dark : Brightness.light,
-        statusBarIconBrightness: green ? Brightness.light : Brightness.dark,
-        systemNavigationBarIconBrightness: Brightness.dark,
-        systemStatusBarContrastEnforced: true,
-        systemNavigationBarContrastEnforced: true,
-      ),
-    );
+    // SystemChrome.setSystemUIOverlayStyle(
+    //   SystemUiOverlayStyle(
+    //     statusBarColor: green ? appTheme.green : appTheme.gray,
+    //     systemNavigationBarColor: appTheme.gray,
+    //     systemNavigationBarDividerColor: appTheme.gray,
+    //     statusBarBrightness: green ? Brightness.dark : Brightness.light,
+    //     statusBarIconBrightness: green ? Brightness.light : Brightness.dark,
+    //     systemNavigationBarIconBrightness: Brightness.dark,
+    //     systemStatusBarContrastEnforced: true,
+    //     systemNavigationBarContrastEnforced: true,
+    //   ),
+    // );
   }
 }
--- lib/presentation/dashboard/controller/controller.dart
+++ lib/presentation/dashboard/controller/controller.dart
@@ -70,30 +70,31 @@ class DashboardController extends GetxController {
   void setSystemUIOverlayStyle(bool green) {
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
-        /// Sets the background color of the status bar.
-        statusBarColor: green ? appTheme.green : appTheme.gray,
 
-        /// Sets the background color of the system navigation bar.
-        systemNavigationBarColor: appTheme.gray,
+          /// Sets the background color of the status bar.
+          // statusBarColor: green ? appTheme.green : appTheme.gray,
 
-        /// Sets the color of the divider between the navigation bar and the app content.
-        systemNavigationBarDividerColor: appTheme.gray,
+          // /// Sets the background color of the system navigation bar.
+          // systemNavigationBarColor: appTheme.gray,
 
-        /// For iOS: Sets the brightness of the status bar (text and icons) for dark/light background.
-        statusBarBrightness: green ? Brightness.dark : Brightness.light,
+          // /// Sets the color of the divider between the navigation bar and the app content.
+          // systemNavigationBarDividerColor: appTheme.gray,
 
-        /// For Android: Sets the brightness of the status bar icons for dark/light background.
-        statusBarIconBrightness: green ? Brightness.light : Brightness.dark,
+          // /// For iOS: Sets the brightness of the status bar (text and icons) for dark/light background.
+          // statusBarBrightness: green ? Brightness.dark : Brightness.light,
 
-        /// For Android: Sets the brightness of the navigation bar icons for dark/light background.
-        systemNavigationBarIconBrightness: Brightness.dark,
+          // /// For Android: Sets the brightness of the status bar icons for dark/light background.
+          // statusBarIconBrightness: green ? Brightness.light : Brightness.dark,
 
-        /// Enforces a contrast between the status bar content and the status bar background.
-        systemStatusBarContrastEnforced: true,
+          // /// For Android: Sets the brightness of the navigation bar icons for dark/light background.
+          // systemNavigationBarIconBrightness: Brightness.dark,
 
-        /// Enforces a contrast between the navigation bar content and the navigation bar background.
-        systemNavigationBarContrastEnforced: true,
-      ),
+          // /// Enforces a contrast between the status bar content and the status bar background.
+          // systemStatusBarContrastEnforced: true,
+
+          // /// Enforces a contrast between the navigation bar content and the navigation bar background.
+          // systemNavigationBarContrastEnforced: true,
+          ),
     );
   }
 }
